b9fea6b3af5cee2e3c70fd279173fb5a555dd894
amadeusz.hauptmann@gmail.com <amadeusz.hauptmann@gmail.com>
2020-09-20 18:43:57 +0200

    Procfile added v3

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 const port =  3000;
 
 const server = http.createServer(function(req, res){
-    fs.readFile('./src/index.html', 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
+    // fs.readFile('./src/index.html', 'utf8', function (err,data) {
+    //     if (err) {
+    //       return console.log(err);
+    //     }
+    //     console.log(data);
+    // })
+    res.writeHead(200, {'Content-type':'text/html'})
+    fs.readFile('./src/index.html', function(error, data){
+        if(error){
+            res.writeHead(404);
+        }else{
+            res.write(data);
         }
-        console.log(data);
+        res.end();
     })
-    res.end();
 })
 
 server.listen(port, function(error){
